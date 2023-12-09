fd3dc0446a50b75a68b5def158e52b4f59b4df08
AhmedMaherFathy <ahmedmaher.xd8551@gmail.com>
2023-12-09 16:47:13 +0200

    first session

---
+++ app/Repositories/Post/Eloquent/EloquentPostRepository.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+namespace App\Repositories\Post\Eloquent;
+
+use App\Repositories\EloquentBaseRepository;
+use App\Repositories\Post\PostRepository;
+
+class EloquentPostRepository extends EloquentBaseRepository implements PostRepository{
+
+}
